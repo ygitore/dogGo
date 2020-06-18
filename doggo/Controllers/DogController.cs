f54e5875fe8bd07f0075b0004653bdc8d3552504
yitbarekgitore <yitbareky@gmail.com>
2020-06-17 22:39:51 -0500

    displaying all dogs

---
+++ doggo/Controllers/DogController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using doggo.Models;
+using doggo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace doggo.Controllers
+{
+    public class DogController : Controller
+    {
+        // GET: DogController
+        private DogRepository _dogRepository;
+        public DogController(IConfiguration conifg)
+        {
+            _dogRepository = new DogRepository(conifg);
+        }
+        public ActionResult Index()
+        {
+            List<Dog> dogs = _dogRepository.GetAllDogs();
+            return View(dogs);
+        }
+
+        // GET: DogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ doggo/Models/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace doggo.Models
+{
+    public class Dog
+    {
+        public int Id;
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public Owner owner { get; set; }
+        public string Breed { get; set; }
+        public string Notes { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
